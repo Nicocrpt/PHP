25c14d57d7e9c1ba0d8b7d5b114a5b88d35a0072
Nicolas Carpita <nicolascarpita@Nicolass-MacBook-Pro.local>
2024-04-18 16:02:44 +0200

    modifs + essaie constructeur

--- pooVoiture.php
+++ pooVoiture.php
@@ -17,7 +17,15 @@
         public function setKm($km) {$this->_km = $km;}
         public function getKm() {return $this->_km;}
 
-        public function getAll() {return "$this->_marque $this->_modele $color - $this->_km km";}
+        public function getAll() {return "<br>$this->_marque $this->_modele $this->color - $this->_km km";}
+
+        public function __voiture($marque, $modele, $couleur, $km)
+        {
+            $this->_marque = $marque ;
+            $this->_modele = $modele ;
+            $this->color = $couleur ;
+            $this->_km = $km ;
+        }
     }
 
 ?>
@@ -26,12 +34,15 @@
 
 
 <?php
+
     $voiture1 = new voiture() ;
+    $voiture2 = new voiture("Dacia", "Sandero", "Rouge", 60000) ;
 
     $voiture1->setMarque("Renault");
     $voiture1->setModele("Clio II");
     $voiture1->color = 'gris(e)' ;
-    $voiture1->setKm(12600) ;
+    $voiture1->setKm(126000) ;
 
     echo $voiture1->getAll() ;
+    echo $voiture2->getAll() ;
 ?>
